2501e8f9d59fe33def4d291e440a1633465dc53e
codewithmaik <>
2023-05-07 10:05:14 +0000

    Fix typo in calculateCorrectAnswer()-function

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,7 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
-        return [operand1 + operand2, "additon"];
+        return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
